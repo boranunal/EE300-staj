2da3261a7cefc20d75edd76523e4308a71ab0119
boranunal <47905529+boranunal@users.noreply.github.com>
2024-07-18 15:44:24 +0300

    Add files via upload

--- auto_test/auto_test.ino
+++ auto_test/auto_test.ino
@@ -1,29 +1,31 @@
 #include <Servo.h>
 #include <cppQueue.h>
+
+//union struct is used to convert incoming bytes to integer
 union arrToInt {
   byte arr[4];
   uint32_t integer;
 };
 arrToInt convert;
+//pin numbers
 const int PE=2;
 const int B=3;
 const int C=4;
+//WLAN switch unoperational
 const int WLAN_SW=7;
 const int POWER=8;
+//LED indicates the test is finished
+const int LED=9;
 const int SERVO=10;
 
 //variables:
 unsigned long msec;
-unsigned long sec;
 byte cmd;
 byte data;
 byte recvd;
-//byte byteArr[700];
+unsigned long repCount = 0;
 int i;
-int temp;
-char reed;
 uint16_t qsize;
-bool runTest = true;
 
 //COMMANDS:
 const byte POWER_OFF = 0x20;
@@ -36,19 +38,22 @@ const byte B_OFF = 0x12;
 const byte C_ON = 0x03;
 const byte C_OFF = 0x13;
 const byte RFID = 0x33;
+const byte REP = 0xC0;
 const byte WLAN_DOWN = 0x51;
 const byte WLAN_UP = 0x52;
 const byte WAIT = 0x0F;
 const byte WAIT_MS = 0x1F;
 
 Servo myservo;
+//command queue
 cppQueue q(1,700,FIFO,true);
-
+//queue for repeating the test
+cppQueue repQ(1,700,FIFO,true);
 void setup() {
   Serial.begin(115200);
   myservo.attach(SERVO);
   servoSetup();
-  //servoSetup();
+  pinMode(LED,OUTPUT);
   pinMode(POWER,OUTPUT);
   digitalWrite(POWER,LOW);
   pinMode(WLAN_SW,OUTPUT);
@@ -67,9 +72,16 @@ void setup() {
 }
 
 void loop() {
-
+  if(q.isEmpty())
+    digitalWrite(LED,HIGH);
+  else
+    digitalWrite(LED,LOW);
+  
   while(!q.isEmpty()){
+    
     q.pop(&cmd);
+    repQ.push(&cmd);//copy the commands into repeat buffer
+    
     switch(cmd){
       case POWER_OFF:
         digitalWrite(POWER,HIGH);
@@ -99,51 +111,119 @@ void loop() {
         digitalWrite(C,HIGH);
         break;
       case WAIT:
+      //convert incoming little endian bytes to integer
+      //while copying the bytes into repeat buffer
+      //if you want to change to big endian, uncomment 
+      //below lines and comment out the current lines
+      //note that I did not check if these work use with attention
+      /*
+        i = 3;
+        while(i>=0){
+          q.pop(&convert.arr[i]);
+          repQ.push(&convert.arr[i--]);
+        }
+       */
         i = 0;
         while(i<4){
-          q.pop(&convert.arr[i++]);
+          q.pop(&convert.arr[i]);
+          repQ.push(&convert.arr[i++]);
         }
-        Serial.println(convert.integer);
         msec = convert.integer;
         delay(1000*msec);
         break;
       case WAIT_MS:
+      //convert incoming little endian bytes to integer
+      //while copying the bytes into repeat buffer
+      //if you want to change to big endian, uncomment 
+      //below lines and comment out the current lines
+      //note that I did not check if these work use with attention
+      /*
+        i = 3;
+        while(i>=0){
+          q.pop(&convert.arr[i]);
+          repQ.push(&convert.arr[i--]);
+        }
+       */
         i = 0;
         while(i<4){
-          q.pop(&convert.arr[i++]);
+          q.pop(&convert.arr[i]);
+          repQ.push(&convert.arr[i++]);
         }
         msec = convert.integer;
-        Serial.println(msec);
         delay(msec);
         break;
       case RFID:
         rfidScan();
         break;
+      //WLAN commands currently unoperational
       case WLAN_DOWN:
         digitalWrite(WLAN_SW,LOW);
         break;
       case WLAN_UP:
         digitalWrite(WLAN_SW,HIGH);
         break;
+      /////////////////////////////
+      case REP:
+      //convert incoming little endian bytes to integer
+      //while decrementing and restoring into the repeat buffer
+      //uncomment the lines below for big endian format
+      //note that I did not check if these work use with attention
+      /*
+        i = 3;
+        while(i>=0){
+          q.pop(&convert.arr[i--]);
+        }
+        repCount=convert.integer;
+        if(repCount>0){
+          convert.integer = --repCount;
+          i=3;
+          while(i>=0)
+            repQ.push(&convert.arr[i--]);
+          copyQ();
+        }
+        break;        
+       
+       */
+        i = 0;
+        while(i<4){
+          q.pop(&convert.arr[i++]);
+        }
+        repCount=convert.integer;
+        if(repCount>0){
+          convert.integer = --repCount;
+          i=0;
+          while(i<4)
+            repQ.push(&convert.arr[i++]);
+          copyQ();
+        }
+        break;
       default:
         break;
     }
   }
 }
-
+//receive commands and put them in a queue
 void recCmds(void){
   int count = 0;
   while(count<qsize){
     if(Serial.available()>0){
       if(Serial.readBytes(&data,1)){
         q.push(&data);
-        Serial.print(recvd);
         count++;
       }
     }
   }
+  Serial.println("success");
 }
-
+//copy the queue from repQ to q
+void copyQ(void){
+  byte copy;
+  while(!repQ.isEmpty()){
+    repQ.pop(&copy);
+    q.push(&copy);
+  }
+}
+//close all the switches to start charging, pretty self explanatory
 void startCharging(void){
   digitalWrite(PE,LOW);
   delay(1000);
@@ -151,22 +231,21 @@ void startCharging(void){
   delay(1000);
   digitalWrite(C,LOW);
 }
+//scans the rfid card 
+//write method for the servo object takes degrees as its argument
+//the values may need changing after re-installing the servo
+//servo needs to be installed in the correct configuration for this to work
 void rfidScan(void){
   myservo.write(180);
-  delay(1500);
+  delay(2000);
   myservo.write(90);
   delay(500);
 }
+//this works only at setup to initialize the servo
+//and serves as a visual indicator for when the arduino resets
 void servoSetup(void){
   myservo.write(0);
   delay(500);
   myservo.write(90);
   delay(500);
-}
-
-void hi(void){
-  myservo.write(0);
-  delay(1000);
-  myservo.write(90);
-  delay(1000);
 }
--- auto_test/test_cmd.py
+++ auto_test/test_cmd.py
@@ -19,78 +19,94 @@ WLAN_DOWN = b'\x51'
 WLAN_UP = b'\x52'
 WAIT = b'\x0f'
 WAIT_MS = b'\x1f'
+REP = b'\xc0'
 END = b'\xff'
 
 
 ##########
 
-def send_byte(byt):
-    arduino.write(byt)
-    sleep(0.5)
-    print(byt)
-
+#return the number of bytes to send.
+#arduino waits for the number of bytes to read.
+#all commands are 1 byte
+#commands with arguments (WAIT, WAIT_MS, REPEAT) consist of
+#5 bytes, 1 for the command 4 for the uint32_t number
 def byte_count(cmds):
     count = 0
     for cmd in cmds:
-        cmd = cmd.strip()
-        cmd = cmd.split()
-        cmd[0] = cmd[0].upper()
-        if (cmd[0] == "WAIT" or cmd[0] == "WAIT_MS"):
-            count = count + 5
-        else:
-            count = count + 1
+        if cmd.strip():
+            cmd = cmd.strip()
+            cmd = cmd.split()
+            cmd[0] = cmd[0].upper()
+            
+            if (cmd[0] == "WAIT" or cmd[0] == "WAIT_MS" or cmd[0]=="REPEAT"):
+                count = count + 5
+            else:
+                count = count + 1
     return count
+#returns a bytearray consisting of commands
+#commands with arguments have the 4 byte argument following
+#the command, argument is a uint32_t number stored in little_endian format
 def makeByteArr(cmds):
     ret = bytes(0)
     for cmd in cmds:
-        print(cmd)
-        cmd = cmd.strip()
-        cmd = cmd.split()
-        cmd[0] = cmd[0].upper()
-        match cmd[0]:
-            case 'POWER_OFF':
-                ret += POWER_OFF
-            case 'POWER_ON':
-                ret += POWER_ON
-            case 'START_CHARGING':
-                ret += START_CHARGING
-            case 'PE_ON':
-                ret += PE_ON
-            case 'PE_OFF':
-                ret += PE_OFF
-            case 'B_ON':
-                ret += B_ON
-            case 'B_OFF':
-                ret += B_OFF
-            case 'C_ON':
-                ret += C_ON
-            case 'C_OFF':
-                ret += C_OFF
-            case 'WAIT':
-                ret += WAIT
-                ret += int(cmd[1]).to_bytes(4,byteorder='little')
-            case 'WAIT_MS':
-                ret += WAIT_MS
-                ret += int(cmd[1]).to_bytes(4,byteorder='little')
-            case 'RFID':
-                ret += RFID
-            case 'WLAN_UP':
-                ret += WLAN_UP
-            case 'WLAN_DOWN':
-                ret += WLAN_DOWN
-            case default:
-                print("Command not found: "+ cmd[0])
-                input("Transmission aborted! Press enter to quit")
-                exit()
+        if cmd.strip():
+            cmd = cmd.strip()
+            cmd = cmd.split()
+            cmd[0] = cmd[0].upper()
+            print(cmd[0],end=' ')
+            if cmd[0]=='WAIT' or cmd[0]=='WAIT_MS' or cmd[0]=='REPEAT':
+                print(cmd[1])
+            else:
+                print()
+            match cmd[0]:
+                case 'POWER_OFF':
+                    ret += POWER_OFF
+                case 'POWER_ON':
+                    ret += POWER_ON
+                case 'START_CHARGING':
+                    ret += START_CHARGING
+                case 'PE_ON':
+                    ret += PE_ON
+                case 'PE_OFF':
+                    ret += PE_OFF
+                case 'B_ON':
+                    ret += B_ON
+                case 'B_OFF':
+                    ret += B_OFF
+                case 'C_ON':
+                    ret += C_ON
+                case 'C_OFF':
+                    ret += C_OFF
+                case 'WAIT':
+                    ret += WAIT
+                    ret += int(cmd[1]).to_bytes(4,byteorder='little')
+                case 'WAIT_MS':
+                    ret += WAIT_MS
+                    ret += int(cmd[1]).to_bytes(4,byteorder='little')
+                case 'RFID':
+                    ret += RFID
+                case 'WLAN_UP':
+                    ret += WLAN_UP
+                case 'WLAN_DOWN':
+                    ret += WLAN_DOWN
+                case 'REPEAT':
+                    ret += REP
+                    ret += int(cmd[1]).to_bytes(4,byteorder='little')
+                case default:
+                    print("Command not found: "+ cmd[0])
+                    input("Transmission aborted! Press enter to quit")
+                    exit()
                 
     return ret
         
 if __name__ == "__main__":
     global arduino
+    #this part is for selecting com port#
     print("Available ports: ")
     comPorts = serial.tools.list_ports.comports()
     for com in comPorts:
         print(com)
+    #input the port name until it is a valid port name
     invPort = True
     while(invPort):
         comPort = input("Select port (write the name of the port): ")
@@ -100,18 +116,28 @@ if __name__ == "__main__":
         except:
             print("Invalid port name\nEnter something like 'COM5'")
             invPort = True
+    #######################################
+
+    #wait until arduino sends a byte indicating
+    #it is ready for communication
     ready = 0
     while (not ready):
         while(not arduino.in_waiting):
             print(".",end="")
             sleep(0.05)
-        ready = arduino.readline()
+        ready = arduino.readline().strip()
     print("Arduino is ready")
+    #######################################
+
+    #this is for selecting the command file
+    #which should be a .txt file
+    #the command file should be in the same directory
+    #as this .py file
     filesInDir = glob("*.txt")
     print("Select one of the following files:")
     for f in filesInDir:
         print(f)
-
+    #input the file name until it is a valid file name#
     invFile = True
     while(invFile):
         fileName = input("Enter the name of the command file:")
@@ -126,31 +152,27 @@ if __name__ == "__main__":
             except:
                 print("Invalid file name!")
                 invFile = True
-        
+    #######################################   
     cmds = cmdFile.readlines()
     numOfBytes = byte_count(cmds)
-    arduino.write(bytes(str(numOfBytes),'utf-8'))
+    #send the number of bytes to be send
+    arduino.write(bytes(str(numOfBytes),'utf-8')) 
     print(numOfBytes)
+    #this loop is for waiting acknowledgement
     while(not arduino.in_waiting):
         print(".",end="")
         sleep(0.05)
     cmdToSend = makeByteArr(cmds)
     print(cmdToSend)
+    #send the commands
     arduino.write(cmdToSend)
     sleep(0.1)
+    #wait for acknowledge from arduino
     while(not arduino.in_waiting):
         print(".",end="")
         sleep(0.05)
-    read=arduino.read_until('?',numOfBytes)
-    print(read)
-    sleep(5)
-    print(arduino.readline())
-    sleep(5)
-    print(arduino.readline())
-    sleep(5)
-    print(arduino.readline())
-    sleep(5)
-    print(arduino.readline())
+    ack = arduino.readline()
+    print(ack)
     arduino.close()
     input("End of transmission. You may close the program.")
 #### end of main ####          
